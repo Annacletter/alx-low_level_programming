e6a8ccf9234eb8695dbb0aa090cdce2b188a37b7
Annacletter <anna.cletter12@gmail.com>
2022-09-09 16:49:02 -0700

    Numberz

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,39 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <stdlib.h>                                                                                              
 
-/**
-* main - print if the number is positive, zero, or negative
-*
-* Description: using the main function
-* this program will print Complete the source code
-* Return: 0
-*/
-int main(void)
-{
-int n;
+#include <time.h>                                                                                                
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
- /* your code goes there */
-if (n > 0)
-{
-	printf("%i is positive\n", n;
-}
-else if (n == 0)
-{
-	printf("%i is zero\n", n);
-}
-else if (n < 0)
-{
-	printf("%i is negative\n", n);
-}
-return (0);
+#include <stdio.h>                                                                                               
+
+                                                                                                                 
+
+/**                                                                                                              
+
+* main - print if the number is positive, zero, or negative                                                      
+
+*                                                                                                                
+
+* Description: using the main function                                                                           
+
+* this program will print Complete the source code                                                               
+
+* Return: 0                                                                                                      
+
+*/                                                                                                               
+
+int main(void)                                                                                                   
+
+{                                                                                                                
+
+int n;                                                                                                           
+
+                                                                                                                 
+
+srand(time(0));                                                                                                  
+
+n = rand() - RAND_MAX / 2;                                                                                       
+
+ /* your code goes there */                                                                                      
+
+if (n > 0)                                                                                                       
+
+
