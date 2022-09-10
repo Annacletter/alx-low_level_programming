2d6b9ae126250c87f73335c0aeffa9df5f9c148c
Annacletter <anna.cletter12@gmail.com>
2022-09-10 06:10:43 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,39 +1,32 @@
-#include <stdlib.h>                                                                                              
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-#include <time.h>                                                                                                
+/**
+* main - print if number is positive, zero, or negative
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+* */
+int main(void)
+{
+int n;
 
-#include <stdio.h>                                                                                               
-
-                                                                                                                 
-
-/**                                                                                                              
-
-* main - print if the number is positive, zero, or negative                                                      
-
-*                                                                                                                
-
-* Description: using the main function                                                                           
-
-* this program will print Complete the source code                                                               
-
-* Return: 0                                                                                                      
-
-*/                                                                                                               
-
-int main(void)                                                                                                   
-
-{                                                                                                                
-
-int n;                                                                                                           
-
-                                                                                                                 
-
-srand(time(0));                                                                                                  
-
-n = rand() - RAND_MAX / 2;                                                                                       
-
- /* your code goes there */                                                                                      
-
-if (n > 0)                                                                                                       
-
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+	
+if (n > 0)
+{
+printf("%di is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%di is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%di is negative\n", n);
+}
+return (0);
+}
 
