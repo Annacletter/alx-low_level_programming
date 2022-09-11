356dc5c95311d287b47ae4b61d77d7c96c6cc286
Annacletter <anna.cletter12@gmail.com>
2022-09-11 00:37:23 -0700

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,8 @@ int tens = '0';
 for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 {
 for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
-if (!((ones == tens)(tens > ones)))/*eliminates repeatition*/
+{ 
+if (!((ones == tens) || (tens > ones)))/*eliminates repeatition*/
 {
 putchar(tens);
 putchar(ones);
@@ -20,6 +21,9 @@ if (!(ones == '9' && tens == '8'))/*addes comma and space*/
 {
 putchar(',');
 putchar(' ');
+}
+}
+}
 }
 putchar('\n');
 return (0);
