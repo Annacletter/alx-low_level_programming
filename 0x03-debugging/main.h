c4569311924edbcadd0853d268cb4ad44f852697
Annacletter <anna.cletter12@gmail.com>
2022-09-14 22:49:20 -0700

    Multiples

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,36 +5,11 @@
  * Return: Always 0 (Success)
  */
 
-void positive_or_negative(int i);
+int main(void)
+{
+int i;
 
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: one integer
- * @b: two integer
- * @c: three integer
- * Return: largest number
- */
-
-int largest_number(int a, int b, int c);
-
-/**
- * convert_day - converts day of month to day of year, without accounting
- * for leap year
- * @month: month in number format
- * @day: day of month
- * Return: day of year
- */
-
-int convert_day(int month, int day);
-
-/**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
- */
-
-void print_remaining_days(int month, int day, int year);
-#endif
+i = 0;
+positive_or_negative(i);
+return (0);
+}
