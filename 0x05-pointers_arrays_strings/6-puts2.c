b9f9e40cc724f4a58bad0c636a7e0a8de86672c9
Annacletter <anna.cletter12@gmail.com>
2022-09-19 14:42:35 -0700

    Half the lies

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,31 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * _strlen - returns the length of a string
  * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-int len = 0;
-while (*s != '\0')
-{
-len++;
-s++;
-}
-return (len);
-}
-
-/**
- * puts2 - prints every second character of a string, followed by a new line
- * @str: string to print
+ * Return: Always 0 (Success)
  */
 
 void puts2(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
-_putchar(str[i]);
-_putchar('\n');
+int i = 0;
+
+while (*(str + i) != '\0')
+{
+if (i % 2 == 0)
+putchar(*(str + i));
+i++;
+}
+putchar(10);
 }
