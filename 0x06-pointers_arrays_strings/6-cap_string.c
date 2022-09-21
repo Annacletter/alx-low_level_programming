a4ccccae7d0db40ed7efbb2842f3ad1124f179fa
Annacletter <anna.cletter12@gmail.com>
2022-09-21 13:21:41 -0700

    Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * string_toupper - function to change lowercase letters to uppercase
- * @s: string to change
- * Return: returns s
+ * string_toupper - capitalize all letters
+ * @s: String to be capitalized
+ * Return: Capitalized string
  */
 
 char *string_toupper(char *s)
 {
 int i = 0;
 
-while (*(s + i))
+for (i = 0; s[i] != '\0'; i++)
 {
-if (*(s + i) >= 'a' && *(s + i) <= 'z')
-*(s + i) -= 'a' - 'A';
-i++;
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 'a' + 'A';
 }
 return (s);
 }
