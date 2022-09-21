80722bc054830e4b76cb0610eb3dad7fcdee6b44
Annacletter <anna.cletter12@gmail.com>
2022-09-21 12:27:17 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@
  */
 
 void reverse_array(int *a, int n);
-vod print_number(int n);
+void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
