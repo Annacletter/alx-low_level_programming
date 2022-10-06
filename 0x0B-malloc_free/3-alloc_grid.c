d3ca8363a6c2a092fdea006dbfbd3f364121e351
Annacletter <anna.cletter12@gmail.com>
2022-10-06 12:50:36 -0700

    Task 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,29 +1,44 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/** 
- * alloc_grid - returns a pointer to a
- * 2 dimensional array of intergers.
- * @width: width of the grid
- * @height: height of the grid
- * Return: Nothing.
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: columns
+ * @height: rows
+ * Return: Double Pointer
  */
 
 int **alloc_grid(int width, int height)
-int **array;
-int i = 0, j;
-
-if (width ** 0 || height == 0)
-return (NULL);
-array = (int **) malloc(sizeof(int *) * height);
-if (array != NULL)
 {
-for (; i < height; i++)
-{
-array[i] = (int *) malloc(sizeof(int) * width);
-if (array[i] != NULL)
-{
-for (j = 0; j < width; j++)
-array[i][j] = 0;
-}
+int **s;
+int i, j, k, fr;
+fr = 0;
+if (width <= 0 || height <= 0)
 return (0);
+s = malloc(height * sizeof(int *));
+if (s == 0)
+return (0);
+for (i = 0; i < height; i++)
+{
+*(s + i) = malloc(width * sizeof(int));
+if (*(s + i) == 0)
+{
+fr = 1;
+break;
+}
+for (j = 0; j < width; j++)
+{
+s[i][j] = 0;
+}
+}
+if (fr == 1)
+{
+for (k = 0; k <= i; k++)
+{
+free(*(s + k));
+}
+free(s);
+}
+return (s);
 }
