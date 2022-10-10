4785ea060f072b156555940587553d36288d1abc
Annacletter <anna.cletter12@gmail.com>
2022-10-10 10:26:41 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,81 +1,44 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * new_dog - create new data structure for dog
- * @name: name of dog
- * @age: age of dog
- * @owner: dog owner
- * Return: Pointer to new dog
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Return: struct dog.
+ * if fails, returns NULL.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
-char *cpname, *cpowner;
-int lenname, lenowner;
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
-return (NULL);
-lenname = _strlen(name);
-lenowner = _strlen(owner);
-cpname = malloc(sizeof(char) * lenname + 1);
-if (cpname == NULL)
+dog_t *p_dog;
+int i, lname, lowner;
+p_dog = malloc(sizeof(*p_dog));
+if (p_dog == NULL || !(name) || !(owner))
 {
-free(cpname);
-free(new_dog);
+free(p_dog);
 return (NULL);
 }
-cpname = _strcpy(cpname, name);
-cpowner = malloc(sizeof(char) * lenowner + 1);
-if (cpowner == NULL)
+for (lname = 0; name[lname]; lname++)
+;
+for (lowner = 0; owner[lowner]; lowner++)
+;
+p_dog->name = malloc(lname + 1);
+p_dog->owner = malloc(lowner + 1);
+if (!(p_dog->name) || !(p_dog->owner))
 {
-free(cpowner);
-free(cpname);
-free(new_dog);
+free(p_dog->owner);
+free(p_dog->name);
+free(p_dog);
 return (NULL);
 }
-cpowner = _strcpy(cpowner, owner);
-new_dog->name = cpname;
-new_dog->age = age;
-new_dog->owner = cpowner;
-return (new_dog);
-}
-
-/**
- * _strlen - determinates the lenght of a string
- * @s: pointer to string
- * Return: the length
- */
-
-int _strlen(char *s)
-{
-int a;
-int len;
-for (a = 0; s[a] != '\0'; a++)
-{
-len++;
-}
-return (a);
-}
-
-/**
- * _strcpy - copies a pointed string
- * @dest: pointer to the destine string
- * @src: pointer to the source string
- * Return: the adress of the destiny string
- */
-
-char *_strcpy(char *dest, char *src)
-{
-int a;
-a = 0;
-while (src[a] != '\0')
-{
-dest[a] = src[a];
-a++;
-}
-dest[a] = '\0';
-return (dest);
+for (i = 0; i < lname; i++)
+p_dog->name[i] = name[i];
+p_dog->name[i] = '\0';
+p_dog->age = age;
+for (i = 0; i < lowner; i++)
+p_dog->owner[i] = owner[i];
+p_dog->owner[i] = '\0';
+return (p_dog);
 }
