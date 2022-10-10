588b7ab978b85e9fec98ce49cce52918faf4b50c
Annacletter <anna.cletter12@gmail.com>
2022-10-10 09:25:43 -0700

    Task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,16 +1,5 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
-#include <stdio.h>
+#ifndef SIZE
 
-/**
- * main - check the code
- * Return: Always 0.
- */
+#define SIZE 1024
 
-int main(void)
-{
-int s;
-s = 98 + SIZE;
-printf("%d\n", s);
-return (0);
-}
+#endif
