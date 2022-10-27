6fd4382573ab04317844fb5c995223857ce48f65
Annacletter <anna.cletter12@gmail.com>
2022-10-27 10:50:52 -0700

    Task 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,15 +1,9 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
-/*
- * File: main.h
- * AUthor: Annacletter
- * Desc: Header file containing prototypes for all functions
- *         written in the 0x14-bit_manipulation directory.
- */
-
 int _putchar(char c);
-udnsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
