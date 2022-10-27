41a181a2999c68529be31d769a4c9f0f27e4db03
Annacletter <anna.cletter12@gmail.com>
2022-10-27 11:20:38 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,11 +10,11 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m, bits = 0;
-	while (xor > 0)
-	{
-		bits += (xor & 1);
-		xor >>= 1;
-	}
-	return (bits);
+unsigned long int xor = n ^ m, bits = 0;
+while (xor > 0)
+{
+bits += (xor & 1);
+xor >>= 1;
+}
+return (bits);
 }
