937805c21e6882bbb5a729d63d7050ea8e56eab3
Annacletter <anna.cletter12@gmail.com>
2022-10-31 16:53:55 -0700

    Tsk 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,7 @@ if (!filename)
 	if (!text_content)
 		text_content = "";
 	for (nletters = 0; text_content[nletters]; nletters++)	
+		;
 	rwr = write(fd, text_content, nletters);
 	if (rwr == -1)
 		return (-1);
