16f5684531dd61b8acf20b2d738a68127fc2cee2
Annacletter <anna.cletter12@gmail.com>
2022-12-08 12:34:17 -0800

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -5,25 +5,24 @@
  * @head: A pointer to the head of the dlistint_t list.
  * @n: The integer for the new node to contain.
  * Return: If the function fails - NULL.
-          Otherwise - the address of the new node.
+ *          Otherwise - the address of the new node.
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
 	
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
-	
 	if (*head != NULL)
 		(*head)->prev = new;
 		*head = new;	
 	
 		return (new);
+
 }
