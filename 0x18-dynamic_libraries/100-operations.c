c6f29af1a9cc68f886114ccc840e3f967230e027
Annacletter <anna.cletter12@gmail.com>
2023-08-21 19:28:05 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition
+ * @a: num1
+ * @b: num2
+ * Return: answer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtraction
+ * @a: num1
+ * @b: num2
+ * Return: answer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication
+ * @a: num1
+ * @b: num2
+ * Return: answer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division
+ * @a: num1
+ * @b: num2
+ * Return: answer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus
+ * @a: num1
+ * @b: num2
+ * Return: answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
