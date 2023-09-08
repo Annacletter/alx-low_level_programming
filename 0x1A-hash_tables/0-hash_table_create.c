8809c3ba6b2054d1992c2f9ec35b0dcf7c0876bd
Annacletter <anna.cletter12@gmail.com>
2023-09-08 09:08:18 -0700

    Hash Tables project

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- *
- * Return: pointer to the newly created hash table
+ * hash_table_create - creates a hash table.
+ * @size: Hash table size
+ * Return: Pointer to the created hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int i;
+	hash_table_t *hash_table = NULL;
+	hash_node_t **array = NULL;
+	unsigned long int index;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
+
 	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
+
+	array = malloc(size * sizeof(hash_node_t *));
+	if (array == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
+
+	for (index = 0; index < size; index++)
+		array[index] = NULL;
+
+
+	hash_table->array = array;
+
 	return (hash_table);
 }
--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,9 @@
-#include "hash_tables.h"
-
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
+ * hash_djb2 - Prints djb2 algorithm
+ * @str: string generate hash value
+ * Return: Always 0.
  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
